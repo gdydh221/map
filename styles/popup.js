c71e08dd26380a8027ba36236a01f5f6f5f62b0d
gdydh221 <perizat.sairbekovaa@gmail.com>
2022-05-17 21:38:10 +0600

    first commit

---
+++ styles/popup.js
@@ -0,0 +1,59 @@
+const openPopUp = document.getElementById('open_pop_up');
+const closePopUp = document.getElementById('pop_up_close');
+const popUp = document.getElementById('pop_up');
+
+openPopUp.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp.classList.add('active');
+})
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+})
+
+const openPopUp2 = document.getElementById('open_pop_up_2');
+const closePopUp2 = document.getElementById('pop_up_close2');
+const popUp2 = document.getElementById('pop_up2');
+
+openPopUp2.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp2.classList.add('active');
+})
+closePopUp2.addEventListener('click', () => {
+    popUp2.classList.remove('active');
+})
+
+const openPopUp3 = document.getElementById('open_pop_up_3');
+const closePopUp3 = document.getElementById('pop_up_close3');
+const popUp3 = document.getElementById('pop_up3');
+
+openPopUp3.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp3.classList.add('active');
+})
+closePopUp3.addEventListener('click', () => {
+    popUp3.classList.remove('active');
+})
+
+const openPopUp4 = document.getElementById('open_pop_up_4');
+const closePopUp4 = document.getElementById('pop_up_close4');
+const popUp4 = document.getElementById('pop_up4');
+
+openPopUp4.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp4.classList.add('active');
+})
+closePopUp4.addEventListener('click', () => {
+    popUp4.classList.remove('active');
+})
+
+const openPopUp5 = document.getElementById('open_pop_up_5');
+const closePopUp5 = document.getElementById('pop_up_close5');
+const popUp5 = document.getElementById('pop_up5');
+
+openPopUp5.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp5.classList.add('active');
+})
+closePopUp5.addEventListener('click', () => {
+    popUp5.classList.remove('active');
+})
